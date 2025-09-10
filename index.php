ae812981c64b9c60679df7c5818a8e9142760786
unknown <142GA05@ARRIAGAINFO.LOCAL>
2025-09-10 12:39:28 +0200

    Primer commit

---
+++ index.php
@@ -0,0 +1,20 @@
+
+<?php 
+$nombre = "nora";
+?>
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <title>PHP test</title>
+    </head>
+    <body>
+        <?php echo '<h1>hello world</h1>'; ?>
+         <?php echo '<p>hello world</p>'; ?>
+         <p>esto es html XD</p>
+          <?php echo '<p>hello world</p>'; ?>
+           <?php echo '<p>hello world</p>'; ?>
+        <?php echo $nombre ?>
+       <?= $nombre ?> 
+    </body>
+</html>
